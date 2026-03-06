0f49ec618b64d1877015032e515459db95d0a436
kotokhin98-netizen <kotokhin98@list.ru>
2026-03-06 13:48:50 +0000

    пара в пятницу

---
+++ c.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <unistd.h>
+#include <cstdlib>
+#include <string>
+#include "stone.h"
+
+using namespace std ;
+int main(){
+
+    get_help();
+
+   /* for(char**env=environ;
+        *env != nullptr;
+        env++){
+            cout<< *env << endl;
+        }
+    int a;
+    pid_t pid=getpid();
+    cout << "pid=" << pid << endl;
+    cout << "Ввести любое значение " << endl;
+     cin >> a;
+        
+        
+        
+
+    }
+   
+    cout << "Hello worold" << endl;
+    
+    cerr << "Ошибка" << endl; */
+
+    return 0;
+
+}
---
+++ stone.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <unistd.h>
+#include "stone.h"
+using namespace std;
+#define author "ABC"
+
+void get_help() {
+    cout << "BYE " <<  author  << endl;
+}
